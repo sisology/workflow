9f73b62dcfd0c719816d666811b10ffe6cbb37b4
siso <gtldhs120@gmail.com>
2024-03-19 14:51:17 +0900

    #5

--- src/main/java/com/spring/chap99workflow/Chap99WorkflowApplication.java
+++ src/main/java/com/spring/chap99workflow/Chap99WorkflowApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Chap99WorkflowApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello world!");
         SpringApplication.run(Chap99WorkflowApplication.class, args);
     }
 
