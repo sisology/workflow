3b88a48a8b7303014dd6038ba859cec598011408
siso <gtldhs120@gmail.com>
2024-03-19 12:43:23 +0900

    first commit

---
+++ src/main/java/com/spring/chap99workflow/Chap99WorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.spring.chap99workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap99WorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap99WorkflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/chap99workflow/Chap99WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.chap99workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap99WorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
